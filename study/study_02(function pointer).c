ce81b4a2965870cab265fa5a5ffcf18729c2cd0f
SEUNGGU KANG <63611804+kang9366@users.noreply.github.com>
2020-09-09 22:55:32 +0900

    Add files via upload

---
+++ study/study_02(function pointer).c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int add(int a, int b);
+int main() {
+	int(*fpt)(int a, int b);
+	int a, b, sum;
+	a = 10;
+	b = 20;
+	fpt = add;
+	sum = fpt(a, b);
+	printf("sum= %d \n", sum);
+	return 0;
+}
+
+int add(int a, int b) {
+	return a + b;
+}
